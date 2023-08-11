80de3a409d954c5435b6fcb1eea0c8c50522e2ee
THAM-SANQA <ndabat020@gmail.com>
2023-08-11 14:55:11 +0200

    Review

--- Chapter8/project8.js
+++ Chapter8/project8.js
@@ -1,21 +1,21 @@
 //   Projects
 //Word scrambler
-let str = "JavaScript";
+// let str = "JavaScript";
 
-function scramble(val) {
-    let max = val.length;
-    let temp = "";
-    for (let i = 0; i < max; i++) {
-        console.log(val.length);
-        let index = Math.floor(Math.random() * val.length);
-        temp += val[index];
-        console.log(temp);
-        val = val.substr(0, index) + val.substr(index + 1);
-        console.log(val);
-    }
-    return temp;
-}
-console.log(scramble(str));
+// function scramble(val) {
+//     let max = val.length;
+//     let temp = "";
+//     for (let i = 0; i < max; i++) {
+//         console.log(val.length);
+//         let index = Math.floor(Math.random() * val.length);
+//         temp += val[index];
+//         console.log(temp);
+//         val = val.substr(0, index) + val.substr(index + 1);
+//         console.log(val);
+//     }
+//     return temp;
+// }
+// console.log(scramble(str));
 
 //Countdown timer
 const endDate = "Sept 19 2023";
