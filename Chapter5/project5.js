256cbf9f112622e3abd38adabfa2e89a0cf22e65
THAM-SANQA <ndabat020@gmail.com>
2023-08-15 16:29:10 +0200

    first commit

--- Chapter5/project5.js
+++ Chapter5/project5.js
@@ -1,7 +1,7 @@
 // Project 5
 // Multiplication Table
 const math = [];
-let multiply = 15;
+let multiply = Number(prompt("Please enter a number for the multiplication table: "));
 for (let x = 0; x < multiply + 1; x++) {
     const temp = [];
     for (let y = 0; y < multiply + 1; y++) {
