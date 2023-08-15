6ee46b3f66cacddf21fe18e95668f3e48f504951
THAM-SANQA <ndabat020@gmail.com>
2023-08-15 09:18:33 +0200

    2nd Commit

---
+++ Chapter13/project13.js
@@ -0,0 +1,33 @@
+let allowed = ["junior", "1738", "idkwtdbby"];
+
+function passwordChecker(pass) {
+    return allowed.includes(pass);
+}
+
+function login(password) {
+    return new Promise((resolve, reject) => {
+        if (passwordChecker(password)) {
+            resolve({
+                status: true
+            });
+        } else {
+            reject ({
+                status: false
+            })
+        }
+    })
+}
+
+function checker(pass) {
+    login(pass)
+    .then(token => {
+        console.log("Approve:");
+        console.log(token);
+    })
+    .catch(value => {
+        console.log("Reject:");
+        console.log(value);
+    })
+}
+checker("sunny");
+checker("password");
